664c19e3e8c21ae6c835c9901071a569529dc662
Georgina Chow <georgina.chow20@imperial.ac.uk>
2024-10-03 12:23:27 +0100

    First code push

---
+++ week1/code/boilerplate.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Author: Georgina Chow georgina.chow20@imperial.ac.uk
+# Script: boilerplate.sh
+# Desc: simple boilerplate.sh 
+# Arguments: none 
+# Date: Oct 2019
+
+echo -e "\nThis is a shell script!\n"
+
+
+#exit
+
+
